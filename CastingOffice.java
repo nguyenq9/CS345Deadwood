83036ac5d113dcfa8714ff9213cb433bfc50d58b
nguyenq9 <nguyenq9@wwu.edu>
2022-11-01 20:24:54 -0700

    All files made

--- CastingOffice.java
+++ CastingOffice.java
@@ -5,8 +5,6 @@
 // |                 |
 // #=================#
 
-public class Scene {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Scene class");
-    }
+public class CastingOffice implements Location {
+
 }
--- Location.java
+++ Location.java
@@ -5,8 +5,7 @@
 // |                 |
 // #=================#
 
-public class Scene {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Scene class");
-    }
+public interface Location {
+    
 }
+
